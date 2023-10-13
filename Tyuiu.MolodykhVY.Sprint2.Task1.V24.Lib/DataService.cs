9ad8e11663a8d03883ff8213b08aabdc887a1c5d
MolodykhVlad <Mr.Prins@yandex.ru>
2023-10-13 10:22:00 +0300

    Task1

---
+++ Tyuiu.MolodykhVY.Sprint2.Task1.V24.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.MolodykhVY.Sprint2.Task1.V24.Lib
+{
+    public class DataService : ISprint2Task1V24
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {   //a = 325, b = 325, c = 242, d = 324
+            //True, False, True, False, True, False
+            bool[] res = new bool[6];
+            res[0] = (a < (d + 10)) & (c > (b - 100));
+            res[1] = (a == b + 1) | (c != d - 82);
+            res[2] = !(!res[0]);
+            res[3] = (a <= b) ^ (c >= d - 100);
+            res[4] = (a == b) || (d > c);
+            res[5] = (Math.Pow(a, 2) < b) && (c - 737 == d);
+            return res;
+        }
+    }
+}
+
---
+++ Tyuiu.MolodykhVY.Sprint2.Task1.V24/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MolodykhVY.Sprint2.Task1.V24.Lib;
+
+namespace Tyuiu.MolodykhVY.Sprint2.Task1.V24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int a = 325;
+            int b = 325;
+            int c = 242;
+            int d = 324;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+            Console.Title = "Спринт #2 | Выполнил: Молодых В. Ю. | ИИПб-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы разветвляющейся структуры                               *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #26                                                             *");
+            Console.WriteLine("* Выполнил: Молодых Владислав Юрьевич | ИИПб-23-3                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,         *");
+            Console.WriteLine("* последовательность можно чередовать, но использовать один раз в         *");
+            Console.WriteLine("* в выражении) и логических операций (|, &, ||, &&, !, ^,                 *");
+            Console.WriteLine("* последовательность операций не должна нарушаться), а также              *");
+            Console.WriteLine("* арифметических выражений, которая вернет логическую последовательность  *");
+            Console.WriteLine("* (True, False, True, False, True, Fals), при a = 325, b = 325, c = 242,  *");
+            Console.WriteLine("* d = 324                                                                 *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("A - " + a);
+            Console.WriteLine("B - " + b);
+            Console.WriteLine("C - " + c);
+            Console.WriteLine("D - " + d);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат                                                               *");
+            Console.WriteLine("***************************************************************************");
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
