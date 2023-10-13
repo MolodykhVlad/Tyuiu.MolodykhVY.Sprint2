ab47504156d62fc0f1c6e2219ead5f81b13c787a
MolodykhVlad <Mr.Prins@yandex.ru>
2023-10-13 11:00:55 +0300

    Task3

---
+++ Tyuiu.MolodykhVY.Sprint2.Task2.V9.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.MolodykhVY.Sprint2.Task2.V9.Lib;
+
+namespace Tyuiu.MolodykhVY.Sprint2.Task2.V9.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 10;
+            int y = 3;
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
