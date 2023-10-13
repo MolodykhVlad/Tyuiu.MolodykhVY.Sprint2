751f705769811d23dce9a8f8c98c3dfb266ae4e5
MolodykhVlad <Mr.Prins@yandex.ru>
2023-10-13 09:59:40 +0300

    Task1

---
+++ Tyuiu.MolodykhVY.Sprint2.Task0.V24.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.MolodykhVY.Sprint2.Task1.V24.Lib;
+
+namespace Tyuiu.MolodykhVY.Sprint2.Task1.V24.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void NotImplementedException()
+        {   
+            DataService ds = new DataService();
+            int a = 325;
+            int b = 325;
+            int c = 242;
+            int d = 324;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+            bool[] wait = new bool[6] { true, false, true, false, true, false };
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
+
