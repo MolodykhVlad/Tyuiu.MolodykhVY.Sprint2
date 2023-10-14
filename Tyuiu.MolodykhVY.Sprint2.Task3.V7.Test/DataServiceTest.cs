ceb89299c0e08f52efe9330f6b08dac95694a74e
MolodykhVlad <Mr.Prins@yandex.ru>
2023-10-14 15:52:40 +0300

    Task3

---
+++ Tyuiu.MolodykhVY.Sprint2.Task3.V7.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.MolodykhVY.Sprint2.Task3.V7.Lib;
+
+namespace Tyuiu.MolodykhVY.Sprint2.Task3.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            var res = ds.Calculate(x);
+            var wait = 50;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
