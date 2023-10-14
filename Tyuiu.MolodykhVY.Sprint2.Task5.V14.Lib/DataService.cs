a2d04d86501d4b9f080f9e048ed0408389075cf0
MolodykhVlad <Mr.Prins@yandex.ru>
2023-10-14 21:16:55 +0300

    Task%

---
+++ Tyuiu.MolodykhVY.Sprint2.Task5.V14.Lib/DataService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.MolodykhVY.Sprint2.Task5.V14.Lib
+{
+    public class DataService : ISprint2Task5V14
+    {
+        public string FindDayName(int k, int d)
+        {
+            string res = "";
+            int temp = 365 % (k + 1);
+            temp -= 1;
+
+           
+
+            switch (temp)
+            {
+                case 1:
+                    res = "Понедельник";
+                    break;
+
+                case 2:
+                    res = "Вторник";
+                    break;
+
+                case 3:
+                    res = "Среда";
+                    break;
+
+                case 4:
+                    res = "Четверг";
+                    break;
+
+                case 5:
+                    res = "Пятница";
+                    break;
+
+                case 6:
+                    res = "Суббота";
+                    break;
+
+                case 7:
+                    res = "Воскресенье";
+                    break;
+
+                default:
+                    throw new ArgumentException($"Значение temp: {temp}");
+
+            }
+
+            
+            return res;
+        }
+    }
+    }
+
+
+
---
+++ Tyuiu.MolodykhVY.Sprint2.Task5.V14/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MolodykhVY.Sprint2.Task5.V14.Lib;
+
+namespace Tyuiu.MolodykhVY.Sprint2.Task5.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Молодых В. Ю. | ИИПб-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Выполнил: Молодых Владислав Юрьевич | ИИПб-23-3                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch вычисляет        *");
+            Console.WriteLine("* требуемое значение и возвращает результат.                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Номер дня в которм вы хотите узнать день недели: ");
+            int k = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(365 % k);
+            Console.Write("Каким днём недели является 1-е января?\n(Ввести цифру 1-7, где 1 - Понедельник, 7 - Воскресенье): ");
+            int d = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.FindDayName(k, d);
+            Console.WriteLine("Ответ: " + res);
+
+            Console.ReadKey();
+        }
+    }
+}
