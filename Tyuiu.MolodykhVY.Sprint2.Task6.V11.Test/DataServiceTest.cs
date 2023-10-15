9a31702b8a31360f9573814fa921a693459f4c50
MolodykhVlad <Mr.Prins@yandex.ru>
2023-10-15 20:05:51 +0300

    Task6

---
+++ Tyuiu.MolodykhVY.Sprint2.Task6.V11.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.MolodykhVY.Sprint2.Task6.V11.Lib;
+
+namespace Tyuiu.MolodykhVY.Sprint2.Task6.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int g = 2013;
+            int m = 11;
+            int n = 17;
+            string wait = (18 + "." + 11 + "." + 2013);
+            string res = ds.FindDateOfNextDay(g, m, n);
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.MolodykhVY.Sprint2.Task6.V11.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MolodykhVY.Sprint2.Task6.V11.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MolodykhVY.Sprint2.Task6.V11.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4b81f0dc-7954-494b-b163-1147ea2c2584")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
